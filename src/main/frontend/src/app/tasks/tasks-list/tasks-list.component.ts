66e5b6072a10b171145c402e02719ceb2f18d81e
konrad.krakowiecki <konrad.krakowiecki@gmail.com>
2019-07-30 18:13:44 +0200

    Adding Tasks List Component to Angular App.

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../task.model';
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.css']
+    selector: 'app-tasks-list',
+    templateUrl: './tasks-list.component.html',
+    styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
 
-  constructor() { }
+    tasks: Task[] = [];
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() {
+        this.tasks.push(new Task(1,"Task1", true, "07/07/2019"));
+        this.tasks.push(new Task(2,"Task2", false, "09/07/2019"));
+        this.tasks.push(new Task(3,"Task3", false, "12/07/2019"));
+    }
+
+    getDueDateLabel(task: Task) {
+        return task.completed ? 'badge-success' : 'badge-primary';
+    }
+
+    onTaskChange(event, task) {
+        console.log("Task has changed");
+        // this.taskService.saveTask(task, event.target.checked).subscribe();
+    }
 }
